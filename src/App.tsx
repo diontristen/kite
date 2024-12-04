1f893b44bda759cf4f0d297fd46fa95c86118687
Dion Tristen Aguilar <53813863+diontristen@users.noreply.github.com>
2024-12-04 14:18:42 +0000

    quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Home = () => <h1>Home Page</h1>;
 
